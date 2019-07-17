ce8aa64f2d9b8ba5d6f41166b60eb9360ed50395
Michael Constantine Dimopoulos <31562759+MichaelDim02@users.noreply.github.com>
2019-07-17 17:24:54 +0000

    Add files via upload

--- hashid.py
+++ hashid.py
@@ -10,17 +10,24 @@ def search(length, chars):
 	conn = sqlite3.connect("files/hashes")
 	cur = conn.cursor()
 	cur.execute("SELECT * FROM hashes WHERE length = ? AND charset = ? ORDER BY popularity DESC", (length, chars))
-	print(Style.BRIGHT + Fore.RED + "Possible Hashes:\n")
+	print("\n" + Style.BRIGHT + Fore.RED + "Possible Hashes:\n")
 	count = 1
 	for row in cur.fetchall():
-		print(Style.BRIGHT + "["+Fore.WHITE+str(count)+Style.BRIGHT + Fore.RED + "] "+Fore.WHITE+str(row[0]))
+		print(Style.BRIGHT + Fore.RED + "["+Fore.WHITE+str(count)+Style.BRIGHT + Fore.RED + "] "+Fore.WHITE+str(row[0]))
 		count = count + 1
-
+	print(" ")
 print("HASHID - Identify the hash!\nBy MihaelDim02")
 
 def extract(hash):
 	length = len(hash.strip())
-	search(length, "a")
+	charset = "a"
+	if "$" in hash:
+		charset = "b"
+	elif "/" in hash:
+		charset = "c"
+	elif hash[:3] == "0x0":
+		charset = "d"
+	search(length, charset)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--hash", help="Hash")

896839aff718a892e0de411504ef0286a5369784
MichaelDim02 <31562759+MichaelDim02@users.noreply.github.com>
2020-05-14 18:17:56 +0000

    Add files via upload

--- hashid.py
+++ hashid.py
@@ -6,6 +6,14 @@ import sqlite3
 def banner():
     print("\n--hash Hash")
 
+def list_():
+    conn = sqlite3.connect("files/hashes")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM hashes")
+    print("\nHoundsniff supports:\n")
+    for row in cur.fetchall():
+        print(row[0])
+    print("Wordpress hash\nMD5 crypt(3)\nSHA256 crypt(3)\nSHA512 crypt(3)")
 def search(length, chars):
     conn = sqlite3.connect("files/hashes")
     cur = conn.cursor()
@@ -21,15 +29,27 @@ print("(\,------'()'--o  Sniff..")
 print(" l_ )  _   /-''    Sniff...")
 print("  /_)_) /_)_)\n")
 ####https://www.asciiart.eu/animals/dogs
-print("Houndsniff - Hash Identification Program - Version 0.2\nBy MCD")
+print("Houndsniff - Hash Identification Program - Version 0.4\nBy MCD")
 
 def extract(hash):
     length = len(hash.strip())
     charset = "a"
     found = False
+
+    #### DEFINITE GUESS #### These if statements are for hashes with definite identifialbe characteristics
     if hash.startswith("$P$"):
         print("\n"+Style.BRIGHT + Fore.RED + "["+Fore.WHITE+"1"+Style.BRIGHT + Fore.RED + "] "+Fore.WHITE+str("Wordpress hash")+"\n")
         found = True
+    elif hash.startswith("$1$"):
+        print("\n"+Style.BRIGHT + Fore.RED + "["+Fore.WHITE+"1"+Style.BRIGHT + Fore.RED + "] "+Fore.WHITE+str("MD5 crypt(3)")+"\n")
+        found = True
+    elif hash.startswith("$5$"):
+        print("\n"+Style.BRIGHT + Fore.RED + "["+Fore.WHITE+"1"+Style.BRIGHT + Fore.RED + "] "+Fore.WHITE+str("SHA256 crypt(3)")+"\n")
+        found = True
+    elif hash.startswith("$6$"):
+        print("\n"+Style.BRIGHT + Fore.RED + "["+Fore.WHITE+"1"+Style.BRIGHT + Fore.RED + "] "+Fore.WHITE+str("SHA512 crypt(3)")+"\n")
+        found = True
+    ### END ###
     elif "$" in hash:
         charset = "b"
     elif "/" in hash:
@@ -39,12 +59,34 @@ def extract(hash):
     if not found:
         search(length, charset)
 
+def about():
+    print("Houndsniff's not a sniffing tool! ;)")
+    print("It is a hash identification & recognition program")
+    print("Version 0.4")
+    print("Written by MCD")
+    print("\nHow it works:")
+    print("Houndsniff uses an sqlite database with hashes")
+    print("and their characteristics and compares them to")
+    print("the characteristics of the given hash. The hases")
+    print("are listed by their popularity, which is determined")
+    print("by its google results in relation to other hashes")
+    print("with the same characteristics.\n\nUse --list to get a list of the supported hashes\n")
+    print("If the hash starts with dollarsign ($) make sure you put it between single quote marks :)")
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--hash", help="Hash")
+parser.add_argument("--about", action="store_true")
+parser.add_argument("--list", action="store_true")
 args = parser.parse_args()
 hash = args.hash
 
 if args.hash:
     extract(hash)
+elif args.about:
+    about()
+elif args.list:
+    list_()
 else:
     banner()
+
+
